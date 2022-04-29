a6694375c57af0aab1fd31941a2a1bf1fea5bab5
marurevi <maryrev@hotmail.com>
2022-04-29 11:35:04 -0300

    Clear all button funcionality

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import './style.css';
 import { savedata, retrivedata } from './modules/localStorage.js';
 
 const entryTask = document.getElementById('newTask');
-//const clearBtn = document.querySelector('.clear');
-
+const clearBtn = document.querySelector('.clear');
+const listOnStorage = JSON.parse(localStorage.getItem('toDoList')) || [];
 
 /* Add new value */
 entryTask.addEventListener('keypress', (e) => {
@@ -14,6 +14,10 @@ entryTask.addEventListener('keypress', (e) => {
   }
 });
 
-
+/* Erase all completed task */
+clearBtn.addEventListener('click', () => {
+  localStorage.setItem('toDoList', JSON.stringify(listOnStorage.filter(item => item.completed === true)));
+  retrivedata();
+})
 
 window.addEventListener('load', retrivedata);
