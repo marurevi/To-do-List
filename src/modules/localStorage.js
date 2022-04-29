8a1d12a0ecdcd0b5e36546d46f8947d014c8a7d8
marurevi <maryrev@hotmail.com>
2022-04-29 13:06:29 -0300

    Fix eslint

--- src/modules/localStorage.js
+++ src/modules/localStorage.js
@@ -1,7 +1,14 @@
-import Task from './taskModule.js';
-
 const entryTask = document.getElementById('newTask');
 
+/* eslint-disable */
+export default class Task {
+  constructor(description, completed = false, index) {
+    this.description = description,
+    this.completed = completed,
+    this.index = index
+  }
+}
+/* elint-enable */
 const storageAvailable = (type) => {
   try {
     const storage = window[type];
@@ -25,7 +32,8 @@ const storageAvailable = (type) => {
 
 export const savedata = () => {
   const listOnStorage = JSON.parse(localStorage.getItem('toDoList')) || [];
-  if (storageAvailable('localStorage')) {
+  if (storageAvailable('localStorage')) 
+    {
     const listElement = new Task(`${entryTask.value}`, false, JSON.parse(`${listOnStorage.length + 1}`));
     listOnStorage.push(listElement);
     localStorage.setItem('toDoList', JSON.stringify(listOnStorage));
--- src/modules/taskmodule.js
+++
@@ -1,7 +0,0 @@
-export default class Task {
-  constructor(description, completed = false, index) {
-    this.description = description,
-    this.completed = completed,
-    this.index = index;
-  }
-}
